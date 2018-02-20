400d0e1d2528a2eca2aacd054e115558e75910cf
Stephen Lorenz <Lorenzsj@clarkson.edu>
2018-02-19 22:21:13 -0500

    Update grader.pl

--- grader.pl
+++ grader.pl
@@ -3,27 +3,46 @@
 use strict;
 use warnings;
 
-my @dirs = grep { -d } glob '*';
+# regex pattern for locating archives
+my $re_archive = qr/\.(zip|tar|tar\.gz|7z)$/;
+my $re_src = qr/\.cpp$/;
 
-foreach my $dir (@dirs) {
-    opendir DIR, $dir or die "Unable to open directory $dir: $!";
-    my @files = readdir DIR;
-    chdir $dir;
-    foreach my $file (@files) {
-        next if ($file eq ".");
-        next if ($file eq "..");
-        my ($ext) = $file =~ /(\.[^.]+)$/;
-        if ($ext eq ".zip") {
-            system("unzip $file");
-            system("rm $file");
-            @files = readdir(DIR);
-        }
-        elsif ($ext eq ".tar") {
-        }
-        elsif ($ext eq ".gz") {
-            system("tar -xvf $file"); 
+# store all directories from current directory
+my @directories = grep(-d , glob '*');
+foreach (@directories) {
+    opendir DIR, $_ or die "Unable to open directory $_: $!";
+    chdir $_ or die "Unable to change directory to $_: $!";
+
+    print "$_\n"; # debug
+    my @archives = grep(${re_archive}, glob '*');
+    if (@archives) {
+        foreach (@archives) { # if archive 
+            my ($ext) = $_ =~ /((\.[^.\s]+)+)$/; # parse file extension
+            if ($ext eq ".zip") {
+                system("unzip $_ > /dev/null 2>&1"); # suppress output
+            }
+            elsif ($ext eq ".tar") {
+                system("tar -xvf $_");
+            }
+            elsif ($ext eq ".tar.gz") {
+                system("tar -xvzf $_");
+            }
+            elsif ($ext eq ".7z") {
+                system("7z e $_ > /dev/null 2>&1"); # supress output
+            }
         }
     }
-    chdir "..";
-    closedir DIR;
+
+    system("g++ -std=c++11 *.cpp &> build.txt");
+
+    chdir ".." or die "Unable to return to parent directory: $!";
+    closedir DIR or die "Unable to close directory $_: $!";
 }
+
+=pod
+    my @files = grep(${re_src}, glob '*');
+    foreach (@files) {
+        print "  $_\n";
+    }
+=cut
+
